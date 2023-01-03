519ca0cf91d0ae9e8915afff438e93dfdcebbfb9
jansu-dev <jan-su-dev@outlook.com>
2023-01-03 20:11:50 +0800

    finish commit backuprequest

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,13 @@ use std::path::Path;
 
 use anyhow::Result;
 use cmd::{build_cli, BackupInfo};
+use futures::channel::mpsc as future_mpsc;
 use futures::StreamExt;
 use kvproto::{
+    brpb::Error_oneof_detail,
     brpb::{BackupRequest, StorageBackend},
     brpb_grpc::BackupClient,
+    kvrpcpb::ApiVersion,
 };
 use mbr::Range;
 // use pd_client::;
@@ -21,7 +24,7 @@ fn main() -> Result<()> {
 
 pub fn run_backup(_backup_cmd: BackupInfo) {
     let pd = "127.0.0.1:2379";
-    let kv = "127.0.0.1:20180";
+    let kv = "127.0.0.1:20160";
     let path = "/Users/suzhipeng/Desktop/dump";
     // let tso = pd_client::current_timestamp(pd);
     // println!("{:#?}", tso);
@@ -42,24 +45,34 @@ pub fn backup_range(range: Range, store_addr: String, external_storage: String) 
     let ch = grpcio::ChannelBuilder::new(env).connect(&store_addr);
     let client = BackupClient::new(ch);
 
-    let mut req = BackupRequest::new();
+    let mut req = BackupRequest::default();
+
+    req.set_cluster_id(7184112658150093742);
+    req.set_rate_limit(1);
     req.set_start_key(range.start_key);
     req.set_end_key(range.end_key);
+    req.set_dst_api_version(ApiVersion::V1);
 
     req.set_start_version(0);
-    req.set_end_version(438366606733082625);
+    req.set_end_version(438500808740569089);
+    req.set_concurrency(1);
+    req.set_compression_type(kvproto::brpb::CompressionType::Lz4);
+    req.set_compression_level(1);
+    req.set_is_raw_kv(false);
+    req.set_cf(String::from("default"));
     // Set an unique path to avoid AlreadyExists error.
     req.set_storage_backend(make_local_backend(&Path::new(&external_storage)));
     let mut stream = client.backup(&req).unwrap();
-    let (tx, rx) = std::sync::mpsc::channel();
+    // let (tx, rx) = std::sync::mpsc::channel();
     client.spawn(async move {
         let resp = stream.next().await;
-        tx.send(resp);
+        println!("{:?}", resp);
+        // tx.send(resp);
     });
 
     loop {
-        let x = rx.recv().unwrap();
-        println!("{:?}", x);
-        std::thread::sleep(std::time::Duration::new(10, 0));
+        // let x = rx.recv().unwrap();
+        // println!("{:?}", x);
+        // std::thread::sleep(std::time::Duration::new(10, 0));
     }
 }
